7ef6c4ebf8a6b016a81b8f333c1260e3d13f991a
Laxman Kumar Mallick <93567151+Laxmanmallick1997@users.noreply.github.com>
2022-06-29 01:12:19 +0530

    Add files via upload

---
+++ IBO/script.js
@@ -0,0 +1,92 @@
+/*
+You have been given a list of products which is having property productName, quantity and description.
+
+
+PROBLEM STATEMENTS:
+
+1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+{
+  "TV": 2,
+  "AC": 2,
+  "FAN": 1
+}
+
+
+
+2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+[{
+    productName: "TV",
+    quantity: 20,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 10,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+     description: "Ceiling Fan"
+  }
+]
+
+*/
+
+
+
+
+const listOfProducts = [{
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan"
+  }
+];
+
+count = function (arr, k) {
+    k = k || String;
+    return arr.reduce(function (counter, item) {
+        var e = k(item);
+        counter[e] = counter.hasOwnProperty(e) ? counter[e] + 1 : 1;
+        return counter;
+    }, {})
+};
+
+
+productName = count(listOfProducts, function (item) {
+    return item.productName
+});
+console.log(productName); // 1st res
+
+const e2 = 'productName';
+
+const key = [...new Map(listOfProducts.map(item =>
+  [item[e2], item])).values()];
+
+console.log(key); //  2nd  res
